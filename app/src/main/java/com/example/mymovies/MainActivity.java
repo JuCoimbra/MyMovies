9dad441bcb95fbb16194e4aa9bbf8ab76a80a3d0
juliane-coimbra <62030952+JuCoimbra@users.noreply.github.com>
2023-05-05 17:31:05 -0300

    Começando a toolbar

--- app/src/main/java/com/example/mymovies/MainActivity.java
+++ app/src/main/java/com/example/mymovies/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mymovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
@@ -10,10 +12,18 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference dbReference = FirebaseDatabase.getInstance().getReference();
+    Toolbar toolbar = findViewById(R.id.toolbar);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String valor = intent.getStringExtra("user");
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Olá");
     }
 }
